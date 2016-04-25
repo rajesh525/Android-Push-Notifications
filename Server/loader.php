1d0d7f009d8adc5c1f22e01688e8ee3d9bd16462
surendra <surendrak96@gmail.com>
2016-04-25 22:33:24 +0530

    commited

--- Server/loader.php
+++ Server/loader.php
@@ -6,5 +6,5 @@ require_once('function.php');
 $conn = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
 // selecting database
 if(!mysql_select_db(DB_DATABASE))
-  print "Not connected with database.";
+  echo "Not connected with database.";
 ?>
